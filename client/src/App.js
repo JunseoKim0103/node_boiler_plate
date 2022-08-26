0fc7e3c73a846ece5663c97730effc3b3fce6ad7
junseokim0103 <hazzisss5249@gmail.com>
2022-08-27 01:37:02 +0900

    boiler-plate 완료 redux, HOC 설정까지

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,21 @@ import {
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Auth from "./hoc/auth.js"
 
 function App() {
+  // const AuthLandingPage = Auth(LandingPage, null);
+  // const AuthLoginPage = Auth(LoginPage, false);
+  // const AuthRegisterPage = Auth(RegisterPage, false);
   return (
     <Router>
       <div>
         <Routes>
+          {/* <Route exact path="/" element = {<AuthLandingPage/>}/>
+          exact path: 정확히 경로가 맞아 떨어져야 실행된다는 의미
+          <Route path="/login" element = {<AuthLoginPage/>}/>
+          <Route path="/register" element = {<AuthRegisterPage/>}/> */}
+
           <Route exact path="/" element = {<LandingPage/>}/>
           {/* exact path: 정확히 경로가 맞아 떨어져야 실행된다는 의미 */}
           <Route path="/login" element = {<LoginPage/>}/>
